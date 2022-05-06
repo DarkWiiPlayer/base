c6aef8831cca8e59bf9ae2d0ffef36baa46474e2
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2022-05-06 10:02:49 +0200

    Make luacheck happy 🎉

--- based/32/generic.lua
+++ based/32/generic.lua
@@ -1,14 +1,13 @@
 local base32 = {}
 
-function base32.decode(base32, lookup)
+function base32.decode(input, lookup)
 	local buffer = {}
-
-	local length = math.floor(#base32*5/8)
-	bits = length*8 - #base32*5
-	acc = 0
-	buffer_pointer = length
-	for i=#base32, 1, -1 do
-		local word5 = lookup[base32:byte(i)]
+	local length = math.floor(#input*5/8)
+	local bits = length*8 - #input*5
+	local acc = 0
+	local buffer_pointer = length
+	for i=#input, 1, -1 do
+		local word5 = lookup[input:byte(i)]
 		word5 = word5 * 2^bits
 		bits = bits + 5
 		acc = acc + word5
