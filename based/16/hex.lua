1b7e814c7e787d5b9111aa384d077812cee1ac43
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2022-05-16 09:44:30 +0200

    Extract inlined gsub functions in hex encoder

--- based/16/hex.lua
+++ based/16/hex.lua
@@ -1,15 +1,19 @@
 local hex = {}
 
+local function encode(char)
+	return string.format('%02x', char:byte())
+end
+
 function hex.encode(data)
-	return (data:gsub(".", function(char)
-		return string.format('%02x', char:byte())
-	end))
+	return (data:gsub(".", encode))
+end
+
+local function decode(byte)
+	return string.char(tonumber(byte, 16))
 end
 
 function hex.decode(data)
-	return (data:gsub("%x%x", function(byte)
-		return string.char(tonumber(byte, 16))
-	end))
+	return (data:gsub("%x%x", decode))
 end
 
 return hex
