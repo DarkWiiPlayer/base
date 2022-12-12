cce88b92e8a6528e6a39c2b5b0718b72f2c13682
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2022-12-12 09:39:29 +0100

    Update spooder taskfile for new spooder version

--- tasks.lua
+++ tasks.lua
@@ -1,36 +1,39 @@
 local spooder = require 'spooder'
 
 spooder.task "test" {
+	depends = { "clean" };
 	description = "Runs tests";
-	function(helper)
-		helper.run([[
-			rm luacov.stats.out
-			luacheck . || exit
-			busted --coverage --lpath '?.lua' || exit
-			luacov -r html based.lua
-		]])
-	end
+	[[
+		luacheck . || exit
+		busted --coverage --lpath '?.lua' || exit
+		luacov -r html based.lua
+	]]
 }
 
 spooder.task "documentation" {
 	description = "Builds and pushes the documentation";
 	depends = { "test"};
-	function(helper)
-		helper.run([[
-			hash=$(git log -1 --format=%h)
-			mkdir -p doc/coverage
-			cp -r luacov-html/* doc/coverage
-			ldoc .
-			cd doc
-				find . | treh -c
-				git add --all
-				if git log -1 --format=%s | grep "$hash$"
-				then git commit --amend --no-edit
-				else git commit -m "Update documentation to $hash"
-				fi
-				git push --force origin doc
-			cd ../
-			git stash pop || true
-		]])
-	end;
+	[[
+		hash=$(git log -1 --format=%h)
+		mkdir -p doc/coverage
+		cp -r luacov-html/* doc/coverage
+		ldoc .
+		cd doc
+			find . | treh -c
+			git add --all
+			if git log -1 --format=%s | grep "$hash$"
+			then git commit --amend --no-edit
+			else git commit -m "Update documentation to $hash"
+			fi
+			git push --force origin doc
+		cd ../
+		git stash pop || true
+	]]
+}
+
+spooder.task "clean" {
+	description = "Delete all build leftovers";
+	'rm -f luacov.stats.out';
+	'rm -f luacov.report.out';
+	'rm -f -r luacov-html';
 }
